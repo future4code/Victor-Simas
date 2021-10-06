3c61dc45555d21f2882d2d0a2ed4c1adba4d5a0a
Victor Brunno Simas Cardoso <victorbscardoso@gmail.com>
2021-10-06 18:06:40 -0300

    atividade do diao

---
+++ strings-arrays/index.js
@@ -0,0 +1,93 @@
+/*   /* Tente responder os exercícios dessa seção sem executar o código. Se ficar muito difícil, pode rodar no seu computador **para analisar e pensar sobre o resultado.** 
+
+ 1.  Indique todas as mensagens impressas no console, **SEM EXECUTAR o programa**.
+    
+    ```jsx
+    let array
+    console.log('a. ', array)
+    
+    array = null
+    console.log('b. ', array)
+    
+    array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    console.log('c. ', array.length)
+    
+    let i = 0
+    console.log('d. ', array[i])
+    
+    array[i+1] = 19
+    console.log('e. ', array)
+    
+    const valor = array[i+6]
+    console.log**('f. ', valor)**
+    ``` */
+ //a.  undefined
+ //b.  null
+ //c.  11
+ //d.  3
+ //e. 3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13
+ //f.  9 
+
+
+ ///2. Leia o código abaixo com atenção 
+    
+
+ ///const frase = prompt("Digite uma frase")
+ 
+ //console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+ 
+ //// Qual será o valor impresso no console se a entrada do usuário for: `"Subi num ônibus em Marrocos"`?
+ 
+ 
+ 
+ ///R/SUBI NUM ÔNIBUS EM MIRROCOS 27
+
+
+
+  //// **Exercícios de escrita de código**
+ 
+          
+
+ /* 1--- */
+
+ /* 
+ const nomeUsuario = prompt("Qual seu nome?")
+ const emailUsuario = prompt("Qual seu email?")
+ const frase = `O e-mail ${emailUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeUsuario}!`
+ console.log(frase) */
+
+
+
+
+/*  2- */
+
+ /* comidasFavoritas = [`Pão`,`Leite`,`Omelete`,`Macarrão`,`Bife`];
+ console.log(comidasFavoritas)
+
+ console.log(`Essas são as minhas comidas preferidas:\n ${comidasFavoritas[0]} \n ${comidasFavoritas[1]}\n ${comidasFavoritas[2]}\n ${comidasFavoritas[3]}\n ${comidasFavoritas[4]}`)
+ 
+ comidaFavorita = prompt("Qual sua comida favorita?")
+ comidasFavoritas[2] = comidaFavorita
+ console.log(comidaFavorita)
+ console.log(`Essas são as minhas comidas preferidas:\n ${comidasFavoritas[0]} \n ${comidasFavoritas[1]}\n ${comidaFavorita}\n ${comidasFavoritas[3]}\n ${comidasFavoritas[4]}`)
+ */
+ 
+/* 
+ 3 */
+
+/*  const listaDeTarefas = []
+const primeiraTarefa = prompt("Diga a primeira tarefa para realizar")
+const segundaTarefa = prompt("Diga a segunda tarefa para realizar")
+const terceiraTarefa = prompt("Diga a terceira tarefa para realizar")
+listaDeTarefas.push(primeiraTarefa, segundaTarefa, terceiraTarefa)
+
+console.log(listaDeTarefas)
+
+const tarefaFeita = prompt("Diga a tarefa q você já fez!entre 0 1 2 ")
+const tarefasConcluidas = listaDeTarefas.splice(tarefaFeita, 1)
+console.log(listaDeTarefas)
+
+
+//// */
+
+
