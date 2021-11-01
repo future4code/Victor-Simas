98e1eb3f23a70a22dc98535516668e641912259f
Victor Brunno Simas Cardoso <victorbscardoso@gmail.com>
2021-11-01 18:54:00 -0300

    trampoq

---
+++ modulo2/jsx-labetube/victor-app/src/App.js
@@ -0,0 +1,68 @@
+import './App.css';
+
+function App() {
+  return (
+
+<header>
+<div class="tela-inteira">
+        <input />
+            <h1>Lab Tube</h1>
+            <input type="text" placeholder="Busca" id="campoDeBusca"></input>
+        
+
+        <main>
+            <nav class="menu-vertical">
+                <ul
+                    < li class="botoes-meunu-vertical"> Início</>
+                    < li class="botoes-meunu-vertical" >Em alta</>;
+                    < li class="botoes-meunu-vertical">Inscrições</>;
+                    <hr />
+                    < li class="botoes-meunu-vertical">Originais</>
+                    < li class="botoes-meunu-vertical">Histórico</>
+               />
+            />
+
+            <section class="painel-de-videos">
+                <div class="box-pagina-principal media1" onclick="reproduzVideo()">
+                    <img src="https://picsum.photos/400/400?a=1 " alt=""></>
+                    <>Título do vídeo</>
+                </div>
+                <div class="box-pagina-principal media2" onclick="reproduzVideo()">
+                    <img src="https://picsum.photos/400/400?a=2 " alt=""></>
+                    <>Título do vídeo</>
+                </div>
+                <div class="box-pagina-principal media3" onclick="reproduzVideo()">
+                    <img src="https://picsum.photos/400/400?a=3 " alt=""></>
+                    <>Título do vídeo</>
+                </div>
+                <div class="box-pagina-principal media4" onclick="reproduzVideo()">
+                    <img src="https://picsum.photos/400/400?a=4 " alt=""></>
+                    <>Título do vídeo</>
+                </div>
+                <div class="box-pagina-principal media5" onclick="reproduzVideo()">
+                    <img src="https://picsum.photos/400/400?a=5 " alt=""></>
+                    <>Título do vídeo</>
+                </div>
+                <div class="box-pagina-principal media6" onclick="reproduzVideo()">
+                    <img src="https://picsum.photos/400/400?a=6 " alt=""></>
+                    <>Título do vídeo</>
+                </div>
+                <div class="box-pagina-principal media7" onclick="reproduzVideo()">
+                    <img src="https://picsum.photos/400/400?a=7 " alt=""></>
+                    <>Título do vídeo</>
+                </div>
+                < class="box-pagina-principal media8" onclick="reproduzVideo()">
+                    <img src="https://picsum.photos/400/400?a=8 " alt=""></>
+                    <>Título do vídeo</>
+                />
+            </section>
+        </main>
+
+        <footer>
+            <h4  "Oi! Eu moro no footer! " />
+        </footer>
+    </div>
+</body>
+  
+
+export default App;
